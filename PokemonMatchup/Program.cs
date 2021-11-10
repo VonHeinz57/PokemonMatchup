ca5a5c91c02e0bfcdc65abe443610b09a40f252c
VonHeinz57 <vonheinz57@gmail.com>
2021-11-10 18:33:58 -0500

    Add error handling for if the user misspells the Pokemon name.

--- PokemonMatchup/Program.cs
+++ PokemonMatchup/Program.cs
@@ -10,13 +10,15 @@ namespace PokemonMatchup
         static void Main(string[] args)
         {
             Console.WriteLine("----------");
+            Console.WriteLine("Welcome to Pokemon Matchup!");
 
             int i = 1;
             while (i > 0)
             {
-                Console.WriteLine("Welcome to Pokemon Matchup! What would you like to do?");
+                Console.WriteLine("What would you like to do?");
                 Console.WriteLine("1: Search for super effective types by enemy name");
                 Console.WriteLine("2: Quit");
+                Console.WriteLine("----------");
 
                 int entry = Convert.ToInt32(Console.ReadLine());
 
@@ -25,45 +27,55 @@ namespace PokemonMatchup
 
 
                     //initialize the pokedex class which should be an array full of pokemon with their info
-
                     Pokemon[] pokedex = Pokedex.InitializePokedex();
 
-                    //sets a variable --> will define the enemyPokemon object in the method
+                    //sets the enemy variable --> will define the enemyPokemon object in the method
                     Console.WriteLine("What Pokemon are you up against?");
                     string enemy = Console.ReadLine();
 
+                    //set this as lower case for cleaner check
                     string enemyForCheck = enemy.ToLower();
 
-                    string enemyType1 = EnemyPokemon.GetEnemyType1(pokedex, enemyForCheck).ToLower();
-                    string enemyType2 = EnemyPokemon.GetEnemyType2(pokedex, enemyForCheck).ToLower();
-
-                    List<string> supers = EnemyPokemon.GetSuperEffective(enemyType1, enemyType2);
-
-                    Console.WriteLine("----------");
-
-                    if (enemyType1 != null && enemyType2 != "")
+                    try
                     {
-                        Console.WriteLine($"Your enemy, {enemy}, is a {enemyType1}/{enemyType2} type");
+
+                        string enemyType1 = EnemyPokemon.GetEnemyType1(pokedex, enemyForCheck).ToLower();
+                        string enemyType2 = EnemyPokemon.GetEnemyType2(pokedex, enemyForCheck).ToLower();
+
+                        List<string> supers = EnemyPokemon.GetSuperEffective(enemyType1, enemyType2);
+
+                        Console.WriteLine("----------");
+
+                        if (enemyType1 != null && enemyType2 != "")
+                        {
+                            Console.WriteLine($"Your enemy, {enemy}, is a {enemyType1}/{enemyType2} type");
+                            Console.WriteLine("----------");
+                        }
+
+                        else if (enemyType1 != null && enemyType2 == "")
+                        {
+                            Console.WriteLine($"Your enemy, {enemy}, is a {enemyType1} type");
+                            Console.WriteLine("----------");
+                        }
+
+                        else
+                            Console.WriteLine($"I can't find that pokemon in the pokedex");
+
+                        Console.WriteLine("You should use Pokemon of the following types.");
+
+                        foreach (string type in supers)
+                        {
+                            Console.WriteLine(type);
+                        }
                         Console.WriteLine("----------");
                     }
 
-                    else if (enemyType1 != null && enemyType2 == "")
+                    catch
                     {
-                        Console.WriteLine($"Your enemy, {enemy}, is a {enemyType1} type");
-                        Console.WriteLine("----------");
-                    }
-
-                    else
                         Console.WriteLine($"I can't find that pokemon in the pokedex");
-
-                    Console.WriteLine("You should use Pokemon of the following types.");
-
-                    foreach (string type in supers)
-                    {
-                        Console.WriteLine(type);
+                        Console.WriteLine("----------");
                     }
-                    Console.WriteLine("----------");
-
+                    
                 }
 
                 else if (entry == 2)
